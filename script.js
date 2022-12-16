f9ad1cb0789dad6ac0b74126ee429af4813b0d2a
ahfercen <ahfercen@gmail.com>
2022-12-15 21:41:30 -0500

    Alternating symbols and overwriting cells fix

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ const board = (() => {
     let _grid = new Array(9);
     let _cells = document.getElementsByClassName("cell");
     const setCell = (player, index) =>{
+        if(_grid[index]){
+            return false;
+        }
         const html = _cells.item(index).children;
         html[0].textContent = player.getSign();
         html[0].classList.remove("cell-content");
         html[0].classList.add("cell-filled");
         _grid[index] = player.getSign();
+        return true;
     }
     const getCells = () =>{
         return _cells;
     }
     const resetBoard = () =>{
-        console.log("reset board");
         _grid = new Array(9);
         for(let cell of _cells){
             cell.children[0].classList.remove("cell-filled");
@@ -43,7 +46,7 @@ const Player = (sign) =>{
 const gameState = (() =>{
     const _player1 = Player("X");
     const _player2 = Player("O");
-    const _currentPlayer = _player1;
+    let _currentPlayer = _player1;
 
     const _restart = document.getElementById("reset");
     _restart.onclick = (() =>{
@@ -51,7 +54,9 @@ const gameState = (() =>{
         _currentPlayer = _player1;
     });
     const _cellClicked = e => {
-        board.setCell(_currentPlayer, e.target.id);
+        if (board.setCell(_currentPlayer, e.target.id)){
+            _nextTurn();
+        }
     }
     Array.from(board.getCells()).forEach(element => {
         element.addEventListener("click",_cellClicked);
@@ -60,4 +65,14 @@ const gameState = (() =>{
     const _instruction = document.getElementById("instruction");
     _instruction.textContent = "Player 1 starts with X"
     _instruction.classList.remove("hidden");
+
+    const _nextTurn = () =>{
+        if(_currentPlayer == _player1){
+            console.log("player 1 turn ended");
+            _currentPlayer = _player2;
+        }else{
+            console.log("player 2 turn ended");
+            _currentPlayer = _player1;
+        }
+    }
 })();
