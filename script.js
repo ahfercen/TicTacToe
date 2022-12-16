962156f09f767838ed332e03c6f7d035008b0a5b
ahfercen <ahfercen@gmail.com>
2022-12-15 21:01:14 -0500

    game board input listeners and board reset

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const board = (() => {
     let _grid = new Array(9);
-    const cells = document.getElementsByClassName("cell");
+    let _cells = document.getElementsByClassName("cell");
     let _counter = 0;
     for (_counter; _counter<9;_counter++){
-        _grid[_counter] = (cells.item(_counter).children);
+        _grid[_counter] = (_cells.item(_counter).children);
     }
-
     const setCell = (player, index) =>{
         const html = _grid[index];
         html[0].textContent = player.getSign();
     }
-
+    const getCells = () =>{
+        return _cells;
+    }
+    const resetBoard = () =>{
+        console.log("reset board");
+    }
     return {
-        setCell
+        setCell,
+        getCells,
+        resetBoard,
     };
 })();
 const Player = (sign) =>{
@@ -29,6 +35,15 @@ const gameState = (() =>{
     const _player1 = Player("X");
     const _player2 = Player("O")
 
-    console.log(_player1.getSign());
-    console.log(_player2.getSign());
+    const _restart = document.getElementById("reset");
+    _restart.onclick = (() =>{
+        board.resetBoard();
+    });
+    const _cellClicked = e => {
+        console.log(e.target);
+    }
+    Array.from(board.getCells()).forEach(element => {
+        element.addEventListener("click",_cellClicked);
+    });
+    
 })();
